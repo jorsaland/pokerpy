39360a9f5c4f2761b4b528870686d2a4a36e0fa8
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-15 00:12:50 -0500

    demo 0.0-C completed

---
+++ tests/demos/demo_0.0-C.py
@@ -0,0 +1,63 @@
+"""
+DEMO 0.0-C
+
+The same poker cycle (hand) of previous 0.0 demos was simulated, importing classes and instances
+defined within the library.
+"""
+
+
+import sys
+sys.path.insert(0, '.')
+
+
+import random
+
+
+from pokerpy.structures import Player, Table
+
+
+# Constants
+
+betting_rounds = ['pre-flop', 'flop', 'turn', 'river']
+
+
+# Test players
+
+player_names = ['Andy', 'Boa', 'Coral', 'Dino']
+
+
+# Playability
+
+def cycle():
+
+    print('======================')
+    print('=== STARTING TABLE ===')
+    print('======================\n')
+
+    print('creating table and players...\n')
+    players = [Player(name) for name in player_names]
+    table = Table(players)
+
+    print('======================')
+    print('=== STARTING CYCLE ===')
+    print('======================\n')
+
+    for betting_round in betting_rounds:
+
+        table.deal(betting_round)
+
+        for player in table.players:
+            player.play(betting_round)
+            table.respond(player)
+    
+    winner = random.choice(player_names)
+    print(f'{winner} wins!')
+
+
+# Run test
+
+def main():
+    cycle()
+
+if __name__ == '__main__':
+    main()
