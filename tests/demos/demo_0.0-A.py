afbb8c5abf1b115d7d1d6df3969c1d2a69540640
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-14 21:38:08 -0500

    demo 0.0-A completed

---
+++ tests/demos/demo_0.0-A.py
@@ -0,0 +1,49 @@
+"""
+DEMO 0.0-A
+
+First playability demo! A Texas Hold'em poker cycle (hand) was simulated by printing player actions
+and dealer responses. The winner is chossen randomly. I tried to make this as abstract as possible.
+This will hopefully be the base for further development.
+"""
+
+
+import random
+
+
+# Constants
+
+betting_rounds = ['pre-flop', 'flop', 'turn', 'river']
+
+
+# Test players
+
+player_names = ['Andy', 'Boa', 'Coral', 'Dino']
+
+
+# Playability
+
+def cycle():
+
+    print('======================')
+    print('=== STARTING CYCLE ===')
+    print('======================\n')
+
+    for betting_round in betting_rounds:
+
+        print(f'\n=== Dealer deals cards for {betting_round} ===\n')
+
+        for player_name in player_names:
+            print(f'{player_name} plays {betting_round}')
+            print('dealer responds\n')
+    
+    winner = random.choice(player_names)
+    print(f'{winner} wins!')
+
+
+# Run test
+
+def main():
+    cycle()
+
+if __name__ == '__main__':
+    main()
