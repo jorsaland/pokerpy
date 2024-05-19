a4e4783a9f03fb5e06397c4cd2b2062eaa0a5e48
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-19 12:30:49 -0500

    constants section added with constants defined in previous demos 0.1-A and 0.1-B

--- pokerpy/__init__.py
+++ pokerpy/__init__.py
@@ -8,5 +8,11 @@ __version__ = '0.0.0'
 
 
 # Content
+from .constants import (
+    ACTION_FOLD,
+    aggressive_actions,
+    valid_actions_not_under_bet,
+    valid_actions_under_bet,
+)
 from .structures import Player, Table
 from . import switches
---
+++ pokerpy/constants/__init__.py
@@ -0,0 +1,6 @@
+"""
+Namespace for constants used in other sections.
+"""
+
+
+from ._action_constants import *
