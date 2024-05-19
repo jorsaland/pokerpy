1c56133075a7ac375b07b1bd013b9308483c3870
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-19 13:58:18 -0500

    BettingRound context manager included in Demo 0.1-C

--- tests/demos/demo_0.1-C.py
+++ tests/demos/demo_0.1-C.py
@@ -21,7 +21,7 @@ import pokerpy as pk
 
 # Constants
 
-betting_rounds = ['pre-flop', 'flop', 'turn', 'river']
+betting_round_names = ['pre-flop', 'flop', 'turn', 'river']
 
 
 # Test players
@@ -48,45 +48,36 @@ def cycle(table: pk.Table):
 
     # Define state variables
 
-    for betting_round in betting_rounds:
+    for betting_round_name in betting_round_names:
 
         # Determine whether cycle should be stopped or not
         if len(table.active_players) == 1:
             break
 
-        print(f'\n=== STARTING {betting_round.upper()} ===\n')
+        print(f'\n=== STARTING {betting_round_name.upper()} ===\n')
 
         table.reset_betting_round_states()
-        table.deal(betting_round)
+        table.deal(betting_round_name)
 
-        for player in table.players:
+        with pk.BettingRound(name=betting_round_name, table=table) as betting_round:
+            for player in betting_round:
 
-            # Determine whether betting round should be stopped or not
-            if len(table.active_players) == 1:
-                print(f'<< ONLY ONE ACTIVE PLAYER ({table.active_players[0].name.upper()})... ENDING ROUND >>')
-                break
-
-            # Determine whether player should be allowed to play or not
-            if player not in table.active_players:
-                print(f'<< {player.name.upper()} ALREADY FOLDED >>')
-                continue
-
-            # Let current player to play
-            action = random.choice(pk.possible_actions)
-            while not pk.action_is_valid(action=action, is_under_bet=table.is_under_bet):
-                print(f'<< INVALID ACTION: {action.upper()} >>')
+                # Let current player to play
                 action = random.choice(pk.possible_actions)
-            print(f'{player.name} {action}s')
+                while not pk.action_is_valid(action=action, is_under_bet=table.is_under_bet):
+                    print(f'<< INVALID ACTION: {action.upper()} >>')
+                    action = random.choice(pk.possible_actions)
+                print(f'{player.name} {action}s')
 
-            # Determine whether round becomes under bet or not
-            if action in pk.aggressive_actions:
-                table.is_under_bet = True
-            
-            # Determine whether the player becomes inactive or not
-            if action == pk.ACTION_FOLD:
-                table.active_players.remove(player)
+                # Determine whether round becomes under bet or not
+                if action in pk.aggressive_actions:
+                    table.is_under_bet = True
+                
+                # Determine whether the player becomes inactive or not
+                if action == pk.ACTION_FOLD:
+                    table.active_players.remove(player)
         
-        print(f'\n=== ENDING {betting_round.upper()} ===\n')
+        print(f'\n=== ENDING {betting_round_name.upper()} ===\n')
     
     if len(table.active_players) > 1:
         table.showdown()
