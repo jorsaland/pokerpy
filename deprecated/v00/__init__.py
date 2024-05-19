a244d326c3680523310a10843ad33b3626145f68
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-19 11:11:26 -0500

    current library version added to a deprecated directory, before modifying Player and Table classes for demo 0.1-C

---
+++ deprecated/v00/__init__.py
@@ -0,0 +1,11 @@
+"""
+PokerPy under development!
+"""
+
+
+# Info
+__version__ = '0.0.0'
+
+
+# Content
+from .structures import Player, Table
---
+++ deprecated/v00/structures/_table.py
@@ -0,0 +1,42 @@
+"""
+Defines the class that represents a poker table.
+"""
+
+
+import random
+
+
+from ._player import Player
+
+
+class Table:
+
+
+    """
+    Represents a poker table and the dealer in charge.
+    """
+
+
+    def __init__(self, players: list[Player]):
+        self.players = players
+
+
+    def deal(self, betting_round: str):
+
+        """
+        Makes the dealer to deal the cards.
+        """
+
+        print(f'Dealer deals cards for {betting_round}.\n')
+
+
+    def showdown(self):
+
+        """
+        Makes the dealer to determine who is the winner among remaining players.
+        """
+
+        print(f'\n=== SHOWDOWN! ===\n')
+
+        winner = random.choice(self.players)
+        print(f'{winner.name} wins!')
