2debdb5b4c66c8da2aa635a1811cab9401825eb6
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-20 14:11:14 -0500

    Card class added to structures section for upcoming Demo 0.3-A

--- pokerpy/structures/__init__.py
+++ pokerpy/structures/__init__.py
@@ -3,5 +3,6 @@ Namespace for structure-like classes.
 """
 
 
+from ._card import Card
 from ._player import Player
 from ._table import Table
---
+++ pokerpy/structures/_card.py
@@ -0,0 +1,36 @@
+"""
+Defines the class that represents a poker card.
+"""
+
+
+from pokerpy.constants import sorted_card_values, sorted_card_suits, unicode_code_point_by_card_suit
+from pokerpy.messages import invalid_card_suit_message, invalid_card_value_message
+
+
+class Card:
+
+
+    """
+    Represents a poker card.
+    """
+
+
+    def __init__(self, value: str, suit: str):
+
+        # Validate input
+        if value not in sorted_card_values:
+            message = invalid_card_value_message.format(valid_values=', '.join(sorted_card_values))
+            raise ValueError(message)
+        if suit not in sorted_card_suits:
+            message = invalid_card_suit_message.format(valid_suits=', '.join(sorted_card_suits))
+            raise ValueError(message)
+
+        # Input variables
+        self.value = value
+        self.suit = suit
+    
+
+    def __str__(self):
+        unicode_code_point = unicode_code_point_by_card_suit[self.suit]
+        pretty_suit = chr(unicode_code_point)
+        return f'[{self.value}{pretty_suit}]'
