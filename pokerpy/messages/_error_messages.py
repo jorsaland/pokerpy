dd0100152a674fb85f4b03a04c713a9a34e66ba3
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-20 17:43:04 -0500

    error messages updated for Card class

--- pokerpy/messages/_error_messages.py
+++ pokerpy/messages/_error_messages.py
@@ -6,6 +6,9 @@ Defines messages used in exceptions.
 exiting_unended_betting_round_message = 'Betting round did not end.'
 invalid_card_suit_message = 'Card suits must be one of these: {valid_suits}'
 invalid_card_value_message = 'Card values must be one of these: {valid_values}'
+joker_card_message = 'We live in a society.'
+not_five_cards_hand_message = 'Hands must have exactly five cards.'
 overloaded_betting_round_message = 'Betting round ended before parsing all actions.'
+repeated_cards_hand_message = 'Cards in a hand cannot be repeated.'
 starting_already_ended_betting_round_message = 'Betting round has already ended. It cannot start over.'
 undefined_action_message = "'{action}' is not a betting-round action."
