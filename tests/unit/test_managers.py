386e3d27ec01ee98ce8a610a3d4ab1b211413038
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-19 23:24:21 -0500

    unit test added for testing how different actions are chained

--- tests/unit/test_managers.py
+++ tests/unit/test_managers.py
@@ -186,5 +186,247 @@ class TestBettingRound(TestCase):
         self.assertEqual(cm.exception.args[0], pk.messages.overloaded_betting_round_message)
 
 
+    def test_action_chain(self):
+
+
+        """
+        Runs test cases to check a betting round can correctly chain multiple actions.
+        """
+
+
+        def all_check():
+
+            self.table.activate_all_players()
+            awaited_players: list[pk.Player] = []
+
+            with pk.BettingRound(name='round', table=self.table) as betting_round:
+
+                player = next(betting_round) # Andy
+                player.request(pk.ACTION_CHECK)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Boa
+                player.request(pk.ACTION_CHECK)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Coral
+                player.request(pk.ACTION_CHECK)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Dino
+                player.request(pk.ACTION_CHECK)
+                awaited_players.append(player)
+
+            awaited_player_names = [player.name for player in awaited_players]
+            return awaited_player_names
+
+        self.assertEqual(all_check(), ['Andy', 'Boa', 'Coral', 'Dino'])
+
+
+        def bet_and_folds():
+
+            self.table.activate_all_players()
+            awaited_players: list[pk.Player] = []
+
+            with pk.BettingRound(name='round', table=self.table) as betting_round:
+
+                player = next(betting_round) # Andy
+                player.request(pk.ACTION_BET)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Boa, responding to Andy
+                player.request(pk.ACTION_FOLD)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Coral, responding to Andy
+                player.request(pk.ACTION_FOLD)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Dino, responding to Andy
+                player.request(pk.ACTION_FOLD)
+                awaited_players.append(player)
+
+            awaited_player_names = [player.name for player in awaited_players]
+            return awaited_player_names
+
+        self.assertEqual(bet_and_folds(), ['Andy', 'Boa', 'Coral', 'Dino'])
+
+
+        def bet_and_calls():
+
+            self.table.activate_all_players()
+            awaited_players: list[pk.Player] = []
+
+            with pk.BettingRound(name='round', table=self.table) as betting_round:
+
+                player = next(betting_round) # Andy
+                player.request(pk.ACTION_BET)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Boa, responding to Andy
+                player.request(pk.ACTION_CALL)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Coral, responding to Andy
+                player.request(pk.ACTION_CALL)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Dino, responding to Andy
+                player.request(pk.ACTION_CALL)
+                awaited_players.append(player)
+
+            awaited_player_names = [player.name for player in awaited_players]
+            return awaited_player_names
+
+        self.assertEqual(bet_and_calls(), ['Andy', 'Boa', 'Coral', 'Dino'])
+
+
+        def bet_raises_and_folds():
+
+            self.table.activate_all_players()
+            awaited_players: list[pk.Player] = []
+
+            with pk.BettingRound(name='round', table=self.table) as betting_round:
+
+                player = next(betting_round) # Andy
+                player.request(pk.ACTION_BET)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Boa, responding to Andy
+                player.request(pk.ACTION_RAISE)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Coral, responding to Boa
+                player.request(pk.ACTION_FOLD)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Dino, responding to Boa
+                player.request(pk.ACTION_RAISE)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Andy, responding to Dino
+                player.request(pk.ACTION_FOLD)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Boa, responding to Dino
+                player.request(pk.ACTION_RAISE)
+                awaited_players.append(player)
+
+                # Coral already folded
+
+                player = next(betting_round) # Dino, responding to Boa
+                player.request(pk.ACTION_FOLD)
+                awaited_players.append(player)
+
+                # Andy already folded
+
+                # Boa is the only remaining player
+
+            awaited_player_names = [player.name for player in awaited_players]
+            return awaited_player_names
+
+        self.assertEqual(bet_raises_and_folds(), ['Andy', 'Boa', 'Coral', 'Dino', 'Andy', 'Boa', 'Dino'])
+
+
+        def bet_raises_and_calls():
+
+            self.table.activate_all_players()
+            awaited_players: list[pk.Player] = []
+
+            with pk.BettingRound(name='round', table=self.table) as betting_round:
+
+                player = next(betting_round) # Andy
+                player.request(pk.ACTION_BET)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Boa, responding to Andy
+                player.request(pk.ACTION_RAISE)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Coral, responding to Boa
+                player.request(pk.ACTION_CALL)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Dino, responding to Boa
+                player.request(pk.ACTION_RAISE)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Andy, responding to Dino
+                player.request(pk.ACTION_CALL)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Boa, responding to Dino
+                player.request(pk.ACTION_RAISE)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Coral, responding to Boa
+                player.request(pk.ACTION_CALL)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Dino, responding to Boa
+                player.request(pk.ACTION_CALL)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Andy, responding to Boa
+                player.request(pk.ACTION_CALL)
+                awaited_players.append(player)
+
+                # Every player has responded to Boa
+
+            awaited_player_names = [player.name for player in awaited_players]
+            return awaited_player_names
+
+        self.assertEqual(bet_raises_and_calls(), ['Andy', 'Boa', 'Coral', 'Dino', 'Andy', 'Boa', 'Coral', 'Dino', 'Andy'])
+
+
+        def bet_raises_folds_and_calls():
+
+            self.table.activate_all_players()
+            awaited_players: list[pk.Player] = []
+
+            with pk.BettingRound(name='round', table=self.table) as betting_round:
+
+                player = next(betting_round) # Andy
+                player.request(pk.ACTION_BET)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Boa, responding to Andy
+                player.request(pk.ACTION_RAISE)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Coral, responding to Boa
+                player.request(pk.ACTION_FOLD)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Dino, responding to Boa
+                player.request(pk.ACTION_RAISE)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Andy, responding to Dino
+                player.request(pk.ACTION_CALL)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Boa, responding to Dino
+                player.request(pk.ACTION_RAISE)
+                awaited_players.append(player)
+
+                # Coral already folded
+                
+                player = next(betting_round) # Dino, responding to Boa
+                player.request(pk.ACTION_FOLD)
+                awaited_players.append(player)
+
+                player = next(betting_round) # Andy, responding to Boa
+                player.request(pk.ACTION_CALL)
+                awaited_players.append(player)
+
+                # Every remaining player has responded to Boa
+
+            awaited_player_names = [player.name for player in awaited_players]
+            return awaited_player_names
+
+        self.assertEqual(bet_raises_folds_and_calls(), ['Andy', 'Boa', 'Coral', 'Dino', 'Andy', 'Boa', 'Dino', 'Andy'])
+
+
 if __name__ == '__main__':
     main()
