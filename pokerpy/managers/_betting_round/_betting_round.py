d4fd4a4a09461dae30a7979b594fad3eebc8cfdc
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-19 21:58:26 -0500

    player waiting in BettingRound was migrated to another function

--- pokerpy/managers/_betting_round/_betting_round.py
+++ pokerpy/managers/_betting_round/_betting_round.py
@@ -16,6 +16,9 @@ from pokerpy.structures import Player, Table
 from pokerpy.utils import action_is_valid
 
 
+from ._wait_for_player import wait_for_player
+
+
 class BettingRound:
 
 
@@ -91,22 +94,8 @@ class BettingRound:
                     continue
 
                 # Player keeps its turn until selects a valid action
-                while True:
+                action = yield from wait_for_player(player=player, is_under_bet=self.table.is_under_bet)
 
-                    # Wait for player's action
-                    print(f'Waiting for {player.name}...')
-                    yield player
-
-                    # Determine wheter action is valid or not
-                    action = player.requested_action
-                    if action is not None and action_is_valid(action=action, is_under_bet=self.table.is_under_bet):
-                        action_print_message = f'--- {player.name} {action}s ---'.upper()
-                        print('-' * len(action_print_message))
-                        print(action_print_message)
-                        print('-' * len(action_print_message) + '\n')
-                        break
-                    print(f'<< INVALID ACTION: {action.upper()} >>')
-                
                 # Set consequences of aggressive actions
                 if action in aggressive_actions:
                     self.table.is_under_bet = True
