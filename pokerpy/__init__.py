37f9a95c3ba05315e7d2b976dc3239bd29cebd6e
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-21 19:14:28 -0500

    action is valid moved to betting round context and utils directory removed

--- pokerpy/__init__.py
+++ pokerpy/__init__.py
@@ -16,7 +16,6 @@ from .constants import (
     ACTION_RAISE,
     possible_actions,
 )
-from .managers import BettingRound
+from .managers import BettingRound, action_is_valid
 from .structures import Player, Table
-from .utils import action_is_valid
 from . import messages, switches
