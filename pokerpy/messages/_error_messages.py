f7277751f2d75a9b3977fd68018a7a4eb335d9da
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-21 20:15:58 -0500

    error messages separated by context

--- pokerpy/messages/_error_messages.py
+++ pokerpy/messages/_error_messages.py
@@ -3,6 +3,7 @@ Defines messages used in exceptions.
 """
 
 
+# Betting round messages
 exiting_unended_betting_round_message = 'Betting round did not end.'
 overloaded_betting_round_message = 'Betting round ended before parsing all actions.'
 starting_already_ended_betting_round_message = 'Betting round has already ended. It cannot start over.'
