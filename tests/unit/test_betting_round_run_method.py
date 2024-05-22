b404917c3164fdd0e059c6b0d634710a8b88c330
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-22 00:21:26 -0500

    BettingRound unit tests extended to dependencies and separated in multiple files

---
+++ tests/unit/test_betting_round_run_method.py
@@ -0,0 +1,133 @@
+"""
+Defines unit tests on BettingRound class run method.
+"""
+
+
+import sys
+sys.path.insert(0, '.')
+
+
+from unittest import main, TestCase
+
+
+import pokerpy as pk
+
+
+class TestBettingRoundRunMethod(TestCase):
+
+
+    """
+    Runs unit tests on BettingRound class run method.
+    """
+
+
+    player_names = ['Andy', 'Boa', 'Coral', 'Dino']
+    players = [pk.Player(name) for name in player_names]
+    table = pk.Table(players)
+
+
+    def test_parsing(self):
+
+
+        """
+        Runs test cases to check actions are correctly parsed into the generator object returned by run method.
+        """
+
+
+        def parse_as_many_actions_as_expected():
+
+            self.table.activate_all_players()
+            awaited_players: list[pk.Player] = []
+
+            betting_round_cm = pk.BettingRound(name='round', table=self.table)
+            betting_round = betting_round_cm.run()
+
+            player = next(betting_round) # Andy
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(betting_round) # Boa
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(betting_round) # Coral
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(betting_round) # Dino
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            awaited_player_names = [player.name for player in awaited_players]
+            return awaited_player_names
+
+        self.assertEqual(parse_as_many_actions_as_expected(), ['Andy', 'Boa', 'Coral', 'Dino'])
+
+
+        def parse_less_actions_than_expected():
+
+            self.table.activate_all_players()
+            awaited_players: list[pk.Player] = []
+
+            betting_round_cm = pk.BettingRound(name='round', table=self.table)
+            betting_round = betting_round_cm.run()
+
+            player = next(betting_round) # Andy
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(betting_round) # Boa
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(betting_round) # Coral
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            # Dino is missing
+
+            awaited_player_names = [player.name for player in awaited_players]
+            print(f'{awaited_player_names = }')
+            return awaited_player_names
+
+        self.assertEqual(parse_less_actions_than_expected(), ['Andy', 'Boa', 'Coral'])
+
+
+        def parse_more_actions_than_expected():
+
+            self.table.activate_all_players()
+            awaited_players: list[pk.Player] = []
+
+            betting_round_cm = pk.BettingRound(name='round', table=self.table)
+            betting_round = betting_round_cm.run()
+
+            player = next(betting_round) # Andy
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(betting_round) # Boa
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(betting_round) # Coral
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(betting_round) # Dino
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(betting_round) # Unexpected action
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            awaited_player_names = [player.name for player in awaited_players]
+            print(f'{awaited_player_names = }')
+            return awaited_player_names
+
+        with self.assertRaises(StopIteration):
+            parse_more_actions_than_expected()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/unit/test_betting_round_wait_for_player_function.py
@@ -0,0 +1,218 @@
+"""
+Defines unit tests on wait_for_player function.
+"""
+
+
+import sys
+sys.path.insert(0, '.')
+
+
+from unittest import main, TestCase
+
+
+import pokerpy as pk
+
+
+class TestBettingRoundWaitForPlayerFunction(TestCase):
+
+
+    """
+    Runs unit tests on wait_for_player function.
+    """
+
+
+    player = pk.Player('Lugia')
+
+
+    def test_under_bet_parsing(self):
+
+
+        """
+        Runs test cases to check actions are correctly parsed into the generator object when round is under bet.
+        """
+
+
+        def parse_single_valid_action():
+
+            generator = pk.managers.wait_for_player(self.player, is_under_bet=True)
+
+            # Request action
+            player = next(generator)
+            player.request(pk.ACTION_CALL)
+
+            # End iteration and retrieve returned value
+            try:
+                next(generator)
+            except StopIteration as ex:
+                return ex.value
+
+        self.assertEqual(parse_single_valid_action(), pk.ACTION_CALL)        
+
+
+        def parse_single_invalid_action():
+
+            generator = pk.managers.wait_for_player(self.player, is_under_bet=True)
+
+            # Request action
+            player = next(generator)
+            player.request(pk.ACTION_CHECK)
+
+            # End iteration and retrieve returned value
+            try:
+                next(generator)
+            except StopIteration as ex:
+                return ex.value
+
+        self.assertIsNone(parse_single_invalid_action())
+
+
+        def parse_multiple_invalid_actions():
+
+            generator = pk.managers.wait_for_player(self.player, is_under_bet=True)
+
+            # Request actions
+            player = next(generator)
+            player.request(pk.ACTION_CHECK)
+            player = next(generator)
+            player.request(pk.ACTION_BET)
+            player = next(generator)
+            player.request(pk.ACTION_CHECK)
+            player = next(generator)
+            player.request(pk.ACTION_BET)
+
+            # End iteration and retrieve returned value
+            try:
+                next(generator)
+            except StopIteration as ex:
+                return ex.value
+
+        self.assertIsNone(parse_multiple_invalid_actions())       
+
+
+        def parse_multiple_invalid_actions_and_final_valid_action():
+
+            generator = pk.managers.wait_for_player(self.player, is_under_bet=True)
+
+            # Request actions
+            player = next(generator)
+            player.request(pk.ACTION_CHECK)
+            player = next(generator)
+            player.request(pk.ACTION_BET)
+            player = next(generator)
+            player.request(pk.ACTION_CHECK)
+            player = next(generator)
+            player.request(pk.ACTION_BET)
+            player = next(generator)
+            player.request(pk.ACTION_BET)
+            player = next(generator)
+            player.request(pk.ACTION_CALL)
+
+            # End iteration and retrieve returned value
+            try:
+                next(generator)
+            except StopIteration as ex:
+                return ex.value
+
+        self.assertEqual(parse_multiple_invalid_actions_and_final_valid_action(), pk.ACTION_CALL)
+
+
+    def test_not_under_bet_parsing(self):
+
+
+        """
+        Runs test cases to check actions are correctly parsed into the generator object when round is not under bet.
+        """
+
+
+        def parse_single_valid_action():
+
+            generator = pk.managers.wait_for_player(self.player, is_under_bet=False)
+
+            # Request action
+            player = next(generator)
+            player.request(pk.ACTION_CHECK)
+
+            # End iteration and retrieve returned value
+            try:
+                next(generator)
+            except StopIteration as ex:
+                return ex.value
+
+        self.assertEqual(parse_single_valid_action(), pk.ACTION_CHECK)        
+
+
+        def parse_single_invalid_action():
+
+            generator = pk.managers.wait_for_player(self.player, is_under_bet=False)
+
+            # Request action
+            player = next(generator)
+            player.request(pk.ACTION_RAISE)
+
+            # End iteration and retrieve returned value
+            try:
+                next(generator)
+            except StopIteration as ex:
+                return ex.value
+
+        self.assertIsNone(parse_single_invalid_action())
+
+
+        def parse_multiple_invalid_actions():
+
+            generator = pk.managers.wait_for_player(self.player, is_under_bet=False)
+
+            # Request actions
+            player = next(generator)
+            player.request(pk.ACTION_RAISE)
+            player = next(generator)
+            player.request(pk.ACTION_CALL)
+            player = next(generator)
+            player.request(pk.ACTION_FOLD)
+            player = next(generator)
+            player.request(pk.ACTION_RAISE)
+            player = next(generator)
+            player.request(pk.ACTION_CALL)
+            player = next(generator)
+            player.request(pk.ACTION_FOLD)
+
+            # End iteration and retrieve returned value
+            try:
+                next(generator)
+            except StopIteration as ex:
+                return ex.value
+
+        self.assertIsNone(parse_multiple_invalid_actions())
+
+
+        def parse_multiple_invalid_actions_and_final_valid_action():
+
+            generator = pk.managers.wait_for_player(self.player, is_under_bet=False)
+
+            # Request actions
+            player = next(generator)
+            player.request(pk.ACTION_RAISE)
+            player = next(generator)
+            player.request(pk.ACTION_CALL)
+            player = next(generator)
+            player.request(pk.ACTION_FOLD)
+            player = next(generator)
+            player.request(pk.ACTION_RAISE)
+            player = next(generator)
+            player.request(pk.ACTION_CALL)
+            player = next(generator)
+            player.request(pk.ACTION_FOLD)
+            player = next(generator)
+            player.request(pk.ACTION_CHECK)
+
+            # End iteration and retrieve returned value
+            try:
+                next(generator)
+            except StopIteration as ex:
+                return ex.value
+
+        self.assertEqual(parse_multiple_invalid_actions_and_final_valid_action(), pk.ACTION_CHECK)
+
+
+if __name__ == '__main__':
+    main()
