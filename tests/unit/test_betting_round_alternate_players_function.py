caa899741d9db98f63582d899774ff1b97f3dc7d
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-22 15:53:43 -0500

    extended tests in BettingRound

--- tests/unit/test_betting_round_alternate_players_function.py
+++ tests/unit/test_betting_round_alternate_players_function.py
@@ -21,6 +21,182 @@ class TestBettingRoundAlternatePlayersFunction(TestCase):
     """
 
 
+    def test_active_players(self):
+
+
+        """
+        Runs test cases to check only active players take action.
+        """
+
+
+        def activate_all_players():
+
+            player_names = ['Andy', 'Boa', 'Coral', 'Dino', 'Epa', 'Fomi']
+            player_by_name = {name: pk.Player(name) for name in player_names}
+            table = pk.Table(player_by_name.values())
+
+            table.activate_all_players()
+            generator = pk.managers.alternate_players(table)
+            awaited_players: list[pk.Player] = []
+
+            player = next(generator) # Andy
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(generator) # Boa
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(generator) # Coral
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(generator) # Dino
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(generator) # Epa
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(generator) # Fomi
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            # End iteration
+            try:
+                next(generator) # No more players
+            except StopIteration:
+                awaited_player_names = [player.name for player in awaited_players]
+                return awaited_player_names
+
+        self.assertEqual(activate_all_players(), ['Andy', 'Boa', 'Coral', 'Dino', 'Epa', 'Fomi'])
+
+
+        def deactivate_all_players():
+
+            player_names = ['Andy', 'Boa', 'Coral', 'Dino', 'Epa', 'Fomi']
+            player_by_name = {name: pk.Player(name) for name in player_names}
+            table = pk.Table(player_by_name.values())
+
+            generator = pk.managers.alternate_players(table)
+            awaited_players: list[pk.Player] = []
+
+            # End iteration
+            try:
+                next(generator) # No more players
+            except StopIteration:
+                awaited_player_names = [player.name for player in awaited_players]
+                return awaited_player_names
+
+        self.assertEqual(deactivate_all_players(), [])
+
+
+        def only_activate_first_and_last_player():
+
+            player_names = ['Andy', 'Boa', 'Coral', 'Dino', 'Epa', 'Fomi']
+            player_by_name = {name: pk.Player(name) for name in player_names}
+            table = pk.Table(player_by_name.values())
+
+            active_player_names = ['Andy', 'Fomi']
+            table.active_players.clear()
+            table.active_players.extend([player for name, player in player_by_name.items() if name in active_player_names])
+
+            generator = pk.managers.alternate_players(table)
+            awaited_players: list[pk.Player] = []
+
+            player = next(generator) # Andy
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(generator) # Fomi
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            # End iteration
+            try:
+                next(generator) # No more players
+            except StopIteration:
+                awaited_player_names = [player.name for player in awaited_players]
+                return awaited_player_names
+
+        self.assertEqual(only_activate_first_and_last_player(), ['Andy', 'Fomi'])
+
+
+        def only_deactivate_first_and_last_player():
+
+            player_names = ['Andy', 'Boa', 'Coral', 'Dino', 'Epa', 'Fomi']
+            player_by_name = {name: pk.Player(name) for name in player_names}
+            table = pk.Table(player_by_name.values())
+
+            active_player_names = ['Boa', 'Coral', 'Dino', 'Epa']
+            table.active_players.clear()
+            table.active_players.extend([player for name, player in player_by_name.items() if name in active_player_names])
+
+            generator = pk.managers.alternate_players(table)
+            awaited_players: list[pk.Player] = []
+
+            player = next(generator) # Boa
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(generator) # Coral
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(generator) # Dino
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(generator) # Epa
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            # End iteration
+            try:
+                next(generator) # No more players
+            except StopIteration:
+                awaited_player_names = [player.name for player in awaited_players]
+                return awaited_player_names
+
+        self.assertEqual(only_deactivate_first_and_last_player(), ['Boa', 'Coral', 'Dino', 'Epa'])
+
+
+        def activate_interspersed_players():
+
+            player_names = ['Andy', 'Boa', 'Coral', 'Dino', 'Epa', 'Fomi']
+            player_by_name = {name: pk.Player(name) for name in player_names}
+            table = pk.Table(player_by_name.values())
+
+            active_player_names = ['Andy', 'Coral', 'Epa']
+            table.active_players.clear()
+            table.active_players.extend([player for name, player in player_by_name.items() if name in active_player_names])
+
+            generator = pk.managers.alternate_players(table)
+            awaited_players: list[pk.Player] = []
+
+            player = next(generator) # Andy
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(generator) # Coral
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            player = next(generator) # Epa
+            player.request(pk.ACTION_CHECK)
+            awaited_players.append(player)
+
+            # End iteration
+            try:
+                next(generator) # No more players
+            except StopIteration:
+                awaited_player_names = [player.name for player in awaited_players]
+                return awaited_player_names
+
+        self.assertEqual(activate_interspersed_players(), ['Andy', 'Coral', 'Epa'])
+
+
     def test_parsing(self):
 
 
