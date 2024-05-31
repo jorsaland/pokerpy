df30c4d1e47fcfcc9a707dd9effb86162b4f3b68
jorsaland <andresaldarriaga.94@gmail.com>
2024-05-31 13:50:03 -0500

    Virtual env is now built inside main directory, not upper

--- run/demo.sh
+++ run/demo.sh
@@ -1,19 +1,20 @@
 if [ $1 ]
 then version=$1
-else printf "version="; read version
+else printf "version:"; read version
 fi
 
 if [ $2 ]
 then literal=$2
-else printf "literal="; read literal
+else printf "literal:"; read literal
 fi
 
+clear
 filepath=tests/demos/demo-${version}-${literal}.py
 
-echo ---------------------------
-echo $filepath
-echo ---------------------------
+echo --------------------------------------------------------------
+printf "\n$filepath\n\n"
+echo --------------------------------------------------------------
+printf "\n\n\n"
 
-source ../env/bin/activate
-clear
+source ./env/bin/activate
 python $filepath
