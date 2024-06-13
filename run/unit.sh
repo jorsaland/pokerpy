4df5c8e43d98d915267162cdf973cf9f3c670b05
jorsaland <andresaldarriaga.94@gmail.com>
2024-06-13 11:57:34 -0500

    bash scripts now can be executed from any directory

--- run/unit.sh
+++ run/unit.sh
@@ -1,5 +1,16 @@
+# Move to project location
+
+script_location=$(dirname ${0})
+
+cd $script_location
+cd ..
+
+# Run unit tests and exit
+
 source ./env/bin/activate
-
 clear
-
 python -m unittest discover tests/unit
+
+printf "\n\n"
+echo -n "--- ENTER ---"
+read
