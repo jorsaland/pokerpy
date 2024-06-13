2b448727c7f0365ecb15307f1282df71dbe74854
jorsaland <andresaldarriaga.94@gmail.com>
2024-06-13 11:57:34 -0500

    bash scripts now can be executed from any directory

--- run/build_env.sh
+++ run/build_env.sh
@@ -1,2 +1,11 @@
+# Move to project location
+
+script_location=$(dirname ${0})
+
+cd $script_location
+cd ..
+
+# Create virtual environment and revert location
+
 echo "Creating Python 3.12 virtual environment..."
 python3.12 -m venv ./env
