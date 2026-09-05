84e7ee32e9bd66ba9f8df9efe185b613e696570c
jorsaland <andresaldarriaga.94@gmail.com>
2026-09-04 19:16:06 -0500

    feat: updated betting round prompt_player unit tests

--- tests/unit/test_5_3_betting_round_prompt_player_function.py
+++ tests/unit/test_5_3_betting_round_prompt_player_function.py
@@ -16,39 +16,47 @@ from pokerpy import constants, exceptions, engines, messages, structures
 class TestBettingRoundPromptPlayerFunction(TestCase):
 
 
-    """
-    Runs unit tests on prompt_player function.
-    """
+    "Runs unit tests on prompt_player function."
+
+
+    def setUp(self):
+
+        self.setup_players = [
+            structures.Player('Andy', 1000),
+            structures.Player('Boa', 1000),
+            structures.Player('Coral', 1000),
+            structures.Player('Dino', 1000),
+            structures.Player('Epa', 1000),
+            structures.Player('Fomi', 1000),
+        ]
+
+        self.table = structures.Table(self.setup_players, min_bet=100)
+
+        self.Andy = self.setup_players[0]
+        self.Boa = self.setup_players[1]
+        self.Coral = self.setup_players[2]
+        self.Dino = self.setup_players[3]
+        self.Epa = self.setup_players[4]
+        self.Fomi = self.setup_players[5]
 
 
     def test_last_remaining_player(self):
 
+        "Tests prompted player cannot parse an action because is the last one remaining in the hand cycle."
 
-        """
-        Runs test cases where the prompted player cannot parse an action because is the last one remaining in the hand cycle.
-        """
+        self.table.set_current_player(self.Andy)
 
-
-        table = structures.Table(players = [
-            Andy := structures.Player('Andy', 10),
-            Boa := structures.Player('Boa', 10),
-            Coral := structures.Player('Coral', 10),
-            Dino := structures.Player('Dino', 10),     
-        ])
-        table.set_current_player(Andy)
-        table.set_stopping_player(Coral)
-        Boa.mark_is_folded()
-        Coral.mark_is_folded()
-        Dino.mark_is_folded()
+        self.Boa.mark_is_folded()
+        self.Coral.mark_is_folded()
+        self.Dino.mark_is_folded()
+        self.Epa.mark_is_folded()
+        self.Fomi.mark_is_folded()
 
         generator = engines.prompt_player(
-            table = table,
+            table = self.table,
             open_fold_allowed = False,
-            raise_invalid_actions = False,
+            raise_invalid_actions = True,
         )
-
-
-        # Before parsing an action
 
         with self.assertRaises(exceptions.CloseBettingRoundSignal) as context:
             next(generator)
@@ -57,30 +65,17 @@ class TestBettingRoundPromptPlayerFunction(TestCase):
 
     def test_non_closing_folded_player(self):
 
+        "Tests prompted player cannot parse an action because has already folded, given there are more players to listen."
 
-        """
-        Runs test cases where the prompted player cannot parse an action because has already folded, given there are more players to listen.
-        """
+        self.table.set_current_player(self.Andy)
 
-
-        table = structures.Table(players = [
-            Andy := structures.Player('Andy', 10),
-            structures.Player('Boa', 10),
-            Coral := structures.Player('Coral', 10),
-            structures.Player('Dino', 10),       
-        ])
-        table.set_current_player(Andy)
-        table.set_stopping_player(Coral)
-        Andy.mark_is_folded()
+        self.Andy.mark_is_folded()
 
         generator = engines.prompt_player(
-            table = table,
+            table = self.table,
             open_fold_allowed = False,
-            raise_invalid_actions = False,
+            raise_invalid_actions = True,
         )
-
-
-        # Before parsing an action
 
         with self.assertRaises(exceptions.JumpToNextPlayerSignal) as context:
             next(generator)
@@ -89,30 +84,18 @@ class TestBettingRoundPromptPlayerFunction(TestCase):
 
     def test_closing_folded_player(self):
 
+        "Tests prompted player cannot parse an action because has already folded, given there are no more players to listen."
 
-        """
-        Runs test cases where the prompted player cannot parse an action because has already folded, given there are no more players to listen.
-        """
+        self.table.set_current_player(self.Andy)
+        self.table.set_stopping_player(self.Andy)
 
-
-        table = structures.Table(players = [
-            Andy := structures.Player('Andy', 10),
-            structures.Player('Boa', 10),
-            structures.Player('Coral', 10),
-            structures.Player('Dino', 10),       
-        ])
-        table.set_current_player(Andy)
-        table.set_stopping_player(Andy)
-        Andy.mark_is_folded()
+        self.Andy.mark_is_folded()
 
         generator = engines.prompt_player(
-            table = table,
+            table = self.table,
             open_fold_allowed = False,
-            raise_invalid_actions = False,
+            raise_invalid_actions = True,
         )
-
-
-        # Before parsing an action
 
         with self.assertRaises(exceptions.CloseBettingRoundSignal) as context:
             next(generator)
@@ -121,30 +104,17 @@ class TestBettingRoundPromptPlayerFunction(TestCase):
 
     def test_non_closing_all_in_player(self):
 
+        "Tests prompted player cannot parse an action because is already all-in, given there are more players to listen."
 
-        """
-        Runs test cases where the prompted player cannot parse an action because is already all-in, given there are more players to listen.
-        """
+        self.table.set_current_player(self.Andy)
 
-
-        table = structures.Table(players = [
-            Andy := structures.Player('Andy', 10),
-            structures.Player('Boa', 10),
-            Coral := structures.Player('Coral', 10),
-            structures.Player('Dino', 10),       
-        ])
-        table.set_current_player(Andy)
-        table.set_stopping_player(Coral)
-        Andy.decrease_stack(10)
+        self.Andy.decrease_stack(self.Andy.stack)
 
         generator = engines.prompt_player(
-            table = table,
+            table = self.table,
             open_fold_allowed = False,
-            raise_invalid_actions = False,
+            raise_invalid_actions = True,
         )
-
-
-        # Before parsing an action
 
         with self.assertRaises(exceptions.JumpToNextPlayerSignal) as context:
             next(generator)
@@ -153,29 +123,18 @@ class TestBettingRoundPromptPlayerFunction(TestCase):
 
     def test_closing_all_in_player(self):
 
+        "Tests prompted player cannot parse an action because is already all-in, given there are no more players to listen."
 
-        """
-        Runs test cases where the prompted player cannot parse an action because is already all-in, given there are no more players to listen.
-        """
+        self.table.set_current_player(self.Andy)
+        self.table.set_stopping_player(self.Andy)
 
-
-        table = structures.Table(players = [
-            Andy := structures.Player('Andy', 10),
-            structures.Player('Boa', 10),
-            structures.Player('Coral', 10),
-            structures.Player('Dino', 10),       
-        ])
-        table.set_current_player(Andy)
-        table.set_stopping_player(Andy)
-        Andy.decrease_stack(10)
+        self.Andy.decrease_stack(self.Andy.stack)
 
         generator = engines.prompt_player(
-            table = table,
+            table = self.table,
             open_fold_allowed = False,
-            raise_invalid_actions = False,
+            raise_invalid_actions = True,
         )
-
-        # Before parsing an action
 
         with self.assertRaises(exceptions.CloseBettingRoundSignal) as context:
             next(generator)
@@ -184,150 +143,148 @@ class TestBettingRoundPromptPlayerFunction(TestCase):
 
     def test_non_closing_passive_player(self):
 
+        "Tests prompted player parses a passive action, given there are more players to listen."
 
-        """
-        Runs test cases where the prompted player parses a passive action, given there are more players to listen.
-        """
+        actions = [
+            structures.Action(constants.ACTION_CHECK),
+            structures.Action(constants.ACTION_CALL, 100),
+            structures.Action(constants.ACTION_FOLD),
+        ]
 
+        for action in actions:
 
-        table = structures.Table(players = [
-            Andy := structures.Player('Andy', 10),
-            structures.Player('Boa', 10),
-            Coral := structures.Player('Coral', 10),
-            structures.Player('Dino', 10),       
-        ])
-        table.set_current_player(Andy)
-        table.set_stopping_player(Coral)
+            self.setUp()
+            self.table.set_current_player(self.Andy)
 
-        generator = engines.prompt_player(
-            table = table,
-            open_fold_allowed = False,
-            raise_invalid_actions = False,
-        )
+            if action.category == constants.ACTION_CALL:
+                self.table.set_bet_level(100)
+                self.table.set_full_bet_level(100)
 
+            generator = engines.prompt_player(
+                table = self.table,
+                open_fold_allowed = True,
+                raise_invalid_actions = True,
+            )
 
-        # Before parsing an action
+            with self.subTest('before prompt', action=action):
+                self.assertEqual(next(generator), self.Andy)
 
-        self.assertEqual(next(generator), Andy)
-
-
-        # After parsing an action
-
-        Andy.request_action(structures.Action(constants.ACTION_CHECK))
-        with self.assertRaises(StopIteration) as context:
-            next(generator)
-        self.assertIsNone(context.exception.value)
+            self.Andy.request_action(action)
+            with self.subTest('after prompt', action=action):
+                with self.assertRaises(StopIteration) as context:
+                    next(generator)
+                self.assertIsNone(context.exception.value)
 
 
     def test_closing_passive_player(self):
 
+        "Tests prompted player parses a passive action, given there are more players to listen."
 
-        """
-        Runs test cases where the prompted player parses a passive action, given there are more players to listen.
-        """
+        actions = [
+            structures.Action(constants.ACTION_CHECK),
+            structures.Action(constants.ACTION_CALL, 100),
+            structures.Action(constants.ACTION_FOLD),
+        ]
 
+        for action in actions:
 
-        table = structures.Table(players = [
-            Andy := structures.Player('Andy', 10),
-            structures.Player('Boa', 10),
-            structures.Player('Coral', 10),
-            structures.Player('Dino', 10),       
-        ])
-        table.set_current_player(Andy)
-        table.set_stopping_player(Andy)
+            self.setUp()
+            self.table.set_current_player(self.Andy)
+            self.table.set_stopping_player(self.Andy)
 
-        generator = engines.prompt_player(
-            table = table,
-            open_fold_allowed = False,
-            raise_invalid_actions = False,
-        )
+            if action.category == constants.ACTION_CALL:
+                self.table.set_bet_level(100)
+                self.table.set_full_bet_level(100)
 
+            generator = engines.prompt_player(
+                table = self.table,
+                open_fold_allowed = True,
+                raise_invalid_actions = True,
+            )
 
-        # Before parsing an action
+            with self.subTest('before prompt', action=action):
+                self.assertEqual(next(generator), self.Andy)
 
-        self.assertEqual(next(generator), Andy)
-
-
-        # After parsing an action
-
-        Andy.request_action(structures.Action(constants.ACTION_CHECK))
-        with self.assertRaises(exceptions.CloseBettingRoundSignal) as context:
-            next(generator)
-        self.assertEqual(context.exception.cause, messages.signal_passive_stopping_player)
+            self.Andy.request_action(action)
+            with self.subTest('after prompt', action=action):
+                with self.assertRaises(exceptions.CloseBettingRoundSignal) as context:
+                    next(generator)
+                self.assertEqual(context.exception.cause, messages.signal_passive_stopping_player)
 
 
     def test_non_closing_agressive_player(self):
 
+        "Tests prompted player parses an aggressive action, given there are more players to listen."
 
-        """
-        Runs test cases where the prompted player parses an aggressive action, given there are more players to listen.
-        """
+        actions = [
+            structures.Action(constants.ACTION_RAISE, 200),
+            structures.Action(constants.ACTION_BET, 100),
+        ]
 
+        for action in actions:
 
-        table = structures.Table(players = [
-            Andy := structures.Player('Andy', 10),
-            structures.Player('Boa', 10),
-            Coral := structures.Player('Coral', 10),
-            structures.Player('Dino', 10),       
-        ])
-        table.set_current_player(Andy)
-        table.set_stopping_player(Coral)
+            self.setUp()
+            self.table.set_current_player(self.Andy)
 
-        generator = engines.prompt_player(
-            table = table,
-            open_fold_allowed = False,
-            raise_invalid_actions = False,
-        )
+            if action.category == constants.ACTION_RAISE:
+                self.table.set_bet_level(100)
+                self.table.set_full_bet_level(100)
+            else:
+                self.table.set_bet_level(0)
+                self.table.set_full_bet_level(0)
 
+            generator = engines.prompt_player(
+                table = self.table,
+                open_fold_allowed = True,
+                raise_invalid_actions = True,
+            )
 
-        # Before parsing an action
+            with self.subTest('before prompt', action=action):
+                self.assertEqual(next(generator), self.Andy)
 
-        self.assertEqual(next(generator), Andy)
-
-
-        # After parsing an action
-
-        Andy.request_action(structures.Action(constants.ACTION_BET, 1))
-        with self.assertRaises(StopIteration) as context:
-            next(generator)
-        self.assertIsNone(context.exception.value)
+            self.Andy.request_action(action)
+            with self.subTest('after prompt', action=action):
+                with self.assertRaises(StopIteration) as context:
+                    next(generator)
+                self.assertIsNone(context.exception.value)
 
 
     def test_closing_agressive_player(self):
 
+        "Tests prompted player parses an aggressive action, given there are no more players to listen."
 
-        """
-        Runs test cases where the prompted player parses an aggressive action, given there are no more players to listen.
-        """
+        actions = [
+            structures.Action(constants.ACTION_RAISE, 200),
+            structures.Action(constants.ACTION_BET, 100),
+        ]
 
+        for action in actions:
 
-        table = structures.Table(players = [
-            Andy := structures.Player('Andy', 10),
-            structures.Player('Boa', 10),
-            structures.Player('Coral', 10),
-            structures.Player('Dino', 10),       
-        ])
-        table.set_current_player(Andy)
-        table.set_stopping_player(Andy)
+            self.setUp()
+            self.table.set_current_player(self.Andy)
+            self.table.set_stopping_player(self.Andy)
 
-        generator = engines.prompt_player(
-            table = table,
-            open_fold_allowed = False,
-            raise_invalid_actions = False,
-        )
+            if action.category == constants.ACTION_RAISE:
+                self.table.set_bet_level(100)
+                self.table.set_full_bet_level(100)
+            else:
+                self.table.set_bet_level(0)
+                self.table.set_full_bet_level(0)
 
+            generator = engines.prompt_player(
+                table = self.table,
+                open_fold_allowed = True,
+                raise_invalid_actions = True,
+            )
 
-        # Before parsing an action
+            with self.subTest('before prompt', action=action):
+                self.assertEqual(next(generator), self.Andy)
 
-        self.assertEqual(next(generator), Andy)
-
-
-        # After parsing an action
-
-        Andy.request_action(structures.Action(constants.ACTION_BET, 1))
-        with self.assertRaises(StopIteration) as context:
-            next(generator)
-        self.assertIsNone(context.exception.value)
+            self.Andy.request_action(action)
+            with self.subTest('after prompt', action=action):
+                with self.assertRaises(StopIteration) as context:
+                    next(generator)
+                self.assertIsNone(context.exception.value)
 
 
 if __name__ == '__main__':
